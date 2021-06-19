da6633f032ce15ecbe9a8d3f531374e09fccab51
Otavio Augusto Mota <otaviomota14@gmail.com>
2021-06-19 17:19:25 -0300

    Accepted

---
+++ Average 2.py
@@ -0,0 +1,8 @@
+A = float(input())
+B = float(input())
+C = float(input())
+if (A+B+C) > 30 or (A+B+C) < 0:
+    pass
+else:
+    MEDIA = ((A*2)+(B*3)+(C*5))/(2+3+5)
+    print("MEDIA = %.1f" % MEDIA)
