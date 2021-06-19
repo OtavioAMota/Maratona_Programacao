9eeff3676f3319e766940721300b5cb66f5a0c39
Otavio Augusto Mota <otaviomota14@gmail.com>
2021-06-19 19:03:07 -0300

    Accepted

--- Sphere.py
+++ Sphere.py
@@ -1,5 +1,17 @@
-R = int(input())
+line1 = input().split(" ")
 pi = 3.14159
-VOLUME = float((4/3)*pi*R**3)
+A, B, C = line1
+A = float(A)
+B = float(B)
+C = float(C)
 
-print("VOLUME = %.3f" % VOLUME)
+TRIANGULO = (A * C)/2
+print("TRIANGULO: %.3f"%TRIANGULO)
+CIRCULO = pi*C**2
+print("CIRCULO: %.3f"%CIRCULO)
+TRAPEZIO = ((A+B)*C)/2
+print("TRAPEZIO: %.3f"%TRAPEZIO)
+QUADRADO = B**2
+print("QUADRADO: %.3f"%QUADRADO)
+RETANGULO = A*B
+print("RETANGULO: %.3f"%RETANGULO)
