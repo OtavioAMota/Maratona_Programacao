275f8bb76897901a60e5ef20e409e2c977d8f811
Otavio Augusto Mota <otaviomota14@gmail.com>
2021-06-19 17:43:37 -0300

    Accepted

---
+++ Salary.py
@@ -0,0 +1,8 @@
+NUMERO = int(input())
+HORAS_TRAB = int(input())
+SALARIO_HORAS = float(input())
+
+SALARIO_MES = SALARIO_HORAS*HORAS_TRAB
+
+print("NUMBER =",NUMERO)
+print("SALARY = U$ %.2f" % SALARIO_MES)
